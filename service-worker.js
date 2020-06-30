f6e3e55c88a75013f6e97beaa3d63c69e50b6de2
ottokafka <ottokafka@yahoo.com>
2020-06-30 19:59:04 +0000

    build update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f7798c6c19688ef8963a52a82085fcad.js"
+  "/precache-manifest.171dc5a1b022d2da06e95bed2dbdfbc0.js"
 );
 
 self.addEventListener('message', (event) => {
